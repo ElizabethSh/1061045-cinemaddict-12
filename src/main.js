994c4228087c51a0173c2ec87acb50bf4979654c
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-07 12:52:07 +0200

    Создает массив данных о фильмах, добавляет функцию отображения карточек по 5

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import {createMainNavigationTemplate} from "./view/main-navigation.js";
 import {createFilmsSortingTemplate} from "./view/film-sorting.js";
 import {createContentTemplate} from "./view/content.js";
 import {createAllFilmListTemplate} from "./view/all-film-list.js";
+import {createShowMoreButtonTemplate} from "./view/show-more-button.js";
 import {createFilmCardTemplate} from "./view/film-card.js";
 import {createTopRatedListTemplate} from "./view/top-rated-list.js";
 import {createMostCommentedTemplate} from "./view/most-commented-list.js";
@@ -11,14 +12,19 @@ import {createPopupTemplate} from "./view/popup.js";
 import {createFilmInfoTemplate} from "./view/film-info.js";
 import {createFilmDetailsControlsTemplate} from "./view/film-control.js";
 import {createFilmCommentsTemplate} from "./view/comment.js";
+import {generateFilm} from "./mock/film-card-mock";
 
-const MAX_FILMS_CARD = 5;
+const MAX_FILMS_CARD = 22;
+const MAX_FILMS_PER_STEP = 5;
 const MAX_EXTRA_FILMS_CARD = 2;
 
 const siteHeader = document.querySelector(`.header`);
 const siteMain = document.querySelector(`.main`);
 const siteFooter = document.querySelector(`.footer`);
 const footerStatistic = siteFooter.querySelector(`.footer__statistics`);
+
+const films = new Array(MAX_FILMS_CARD).fill().map(generateFilm);
+console.log(films);
 
 const render = (container, template, position) => {
   container.insertAdjacentHTML(position, template);
@@ -39,14 +45,39 @@ render(siteContent, createTopRatedListTemplate(), `beforeend`); // Top Rated fil
 render(siteContent, createMostCommentedTemplate(), `beforeend`);// Most commented films list
 
 const filmsBoard = siteContent.querySelector(`.films-list .films-list__container`);
-for (let i = 0; i < MAX_FILMS_CARD; i++) {
-  render(filmsBoard, createFilmCardTemplate(), `beforeend`);
+for (let i = 0; i < Math.min(films.length, MAX_FILMS_PER_STEP); i++) {
+  render(filmsBoard, createFilmCardTemplate(films[i]), `beforeend`);
+}
+
+if (films.length > MAX_FILMS_PER_STEP) {
+  let renderedFilmAmount = MAX_FILMS_PER_STEP;
+  render(filmsBoard, createShowMoreButtonTemplate(), `afterend`);
+
+  const showMoreButton = siteContent.querySelector(`.films-list__show-more`);
+
+  const showCards = () => {
+    films.slice(renderedFilmAmount, renderedFilmAmount + MAX_FILMS_PER_STEP)
+      .forEach((film) => render(filmsBoard, createFilmCardTemplate(film), `beforeend`));
+
+    renderedFilmAmount += MAX_FILMS_PER_STEP;
+
+    if (films.length <= renderedFilmAmount) {
+      showMoreButton.remove();
+    }
+  };
+
+  const onShowMoreButtonClick = (evt) => {
+    evt.preventDefault();
+    showCards();
+  };
+
+  showMoreButton.addEventListener(`click`, onShowMoreButtonClick); // для дальнейшего удаления этого обработчика
 }
 
 const extraFilmsLists = siteContent.querySelectorAll(`.films-list--extra .films-list__container`);
 extraFilmsLists.forEach(function (it) {
   for (let i = 0; i < MAX_EXTRA_FILMS_CARD; i++) {
-    render(it, createFilmCardTemplate(), `beforeend`);
+    render(it, createFilmCardTemplate(films[i]), `beforeend`);
   }
 });
 
@@ -56,6 +87,6 @@ render(footerStatistic, createMovieStatisticTemplate(), `beforeend`);
 // popup
 render(siteFooter, createPopupTemplate(), `afterend`);
 const filmInfo = document.querySelector(`.form-details__top-container`);
-render(filmInfo, createFilmInfoTemplate(), `beforeend`);
+render(filmInfo, createFilmInfoTemplate(films[0]), `beforeend`);
 render(filmInfo, createFilmDetailsControlsTemplate(), `beforeend`);
-render(filmInfo, createFilmCommentsTemplate(), `afterend`);
+render(filmInfo, createFilmCommentsTemplate(films[0]), `afterend`);
