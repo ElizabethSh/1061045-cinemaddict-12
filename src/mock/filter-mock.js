44c071fa88b5b6724b0bab61274d76142f662df5
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-09 12:50:01 +0200

    Удаляет из маппы с фильтрами поле all

--- src/mock/filter-mock.js
+++ src/mock/filter-mock.js
@@ -1,21 +1,15 @@
+import {capitalizeFirstLetter} from "../utils";
+
 const filmToFilterMap = {
-  all: (films) => films.length,
-  favorites: (films) => films.filter((film) => film.isFavorite).length,
+  favorites: (films) => films.filter((film) => film.isFavorites).length,
   watchlist: (films) => films.filter((film) => film.isWatchlist).length,
   history: (films) => films.filter((film) => film.isHistory).length
-};
-
-const ucFirst = (filterName) => {
-  if (!filterName) {
-    return filterName;
-  }
-  return filterName[0].toUpperCase() + filterName.slice(1);
 };
 
 export const generateFilters = (films) => {
   return Object.entries(filmToFilterMap).map(([filterName, countFilms]) => {
     return {
-      name: ucFirst(filterName),
+      name: capitalizeFirstLetter(filterName),
       count: countFilms(films)
     };
   });
