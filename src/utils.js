f8138ac01e8425c34ca51ee837e40b13040adb3c
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-09 12:09:57 +0200

    Добавляет функцию превращения первой буквы строки в заглавную

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
+// функция превращает первую букву строки в заглавную
+export const capitalizeFirstLetter = (string) => {
+  if (!string) {
+    return string;
+  }
+  return string[0].toUpperCase() + string.slice(1);
+};
+
 // функция генерации случайного числа из диапозона
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
