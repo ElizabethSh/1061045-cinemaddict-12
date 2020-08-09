65ae139e702fe94a6e507d2634d4f23057d58317
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-09 12:11:49 +0200

    Добавляет условие отображать количество фильмов в фильтре не больше 5

--- src/view/main-navigation.js
+++ src/view/main-navigation.js
@@ -1,7 +1,7 @@
 const createFilterItemTemplate = (filter) => {
   const {name, count} = filter;
   return `<a href="#${name}" class="main-navigation__item">${name}
-      <span class="main-navigation__item-count">${count}</span>
+      ${count < 5 ? `<span class="main-navigation__item-count">${count}</span>` : ``}
     </a>`;
 };
 
@@ -10,6 +10,7 @@ export const createMainNavigationTemplate = (filters) => {
   return (
     `<nav class="main-navigation">
       <div class="main-navigation__items">
+        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
         ${filterTemplate}
       </div>
       <a href="#stats" class="main-navigation__additional">Stats</a>
