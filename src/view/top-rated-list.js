29dea989ab0b185f2fdab9b27de39aa55a73c302
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-12 21:52:47 +0200

    Добавляет класс TopRatedList и экспортирует его из модуля

--- src/view/top-rated-list.js
+++ src/view/top-rated-list.js
@@ -1,4 +1,6 @@
-export const createTopRatedListTemplate = () => {
+import {createElement} from "../utils.js";
+
+const createTopRatedListTemplate = () => {
   return (
     `<section class="films-list--extra">
       <h2 class="films-list__title">Top rated</h2>
@@ -7,3 +9,26 @@ export const createTopRatedListTemplate = () => {
     </section>`
   );
 };
+
+export default class TopRatedList {
+  constructor() {
+    this._element = null;
+  }
+
+  _getTemplate() {
+    return createTopRatedListTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this._getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
