6658e961ad40db2b8acf7406bda91dbecde0ea2c
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-12 22:07:50 +0200

    Добавляет класс Content и экспортирует его из модуля

--- src/view/content.js
+++ src/view/content.js
@@ -1,6 +1,30 @@
-export const createContentTemplate = () => {
+import {createElement} from "../utils.js";
+
+const createContentTemplate = () => {
   return (
     `<section class="films">
     </section>`
   );
 };
+
+export default class Content {
+  constructor() {
+    this._element = null;
+  }
+
+  _getTemplate() {
+    return createContentTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this._getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
