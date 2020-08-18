eb435829ae16c84b0fce8b52939ab0cd11c0815b
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-18 18:14:26 +0200

    Добавляет методы для установки обработчиков на постер, заголовок и комментарии

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -29,10 +29,43 @@ export default class FilmCard extends AbstractView {
   constructor(films) {
     super();
     this._films = films;
+    this._posterClickHandler = this._posterClickHandler.bind(this);
+    this._titleClickHandler = this._titleClickHandler.bind(this);
+    this._commentAmountClickHandler = this._commentAmountClickHandler.bind(this);
   }
 
   _getTemplate() {
     return createFilmCardTemplate(this._films);
+  }
+
+  _posterClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.posterClick();
+  }
+
+  _titleClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.titleClick();
+  }
+
+  _commentAmountClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.commentAmountClick();
+  }
+
+  setPosterClickHandler(callback) {
+    this._callback.posterClick = callback;
+    this.getElement().querySelector(`.film-card__poster`).addEventListener(`click`, this._posterClickHandler);
+  }
+
+  setTitleClickHandler(callback) {
+    this._callback.titleClick = callback;
+    this.getElement().querySelector(`.film-card__title`).addEventListener(`click`, this._titleClickHandler);
+  }
+
+  setCommentAmountClickHandler(callback) {
+    this._callback.commentAmountClick = callback;
+    this.getElement().querySelector(`.film-card__comments`).addEventListener(`click`, this._commentAmountClickHandler);
   }
 }
 
