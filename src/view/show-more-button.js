c442d3edee5a87de2efc96a280616b6ba9688c53
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-18 17:40:02 +0200

    Добавляет в класс методы для установки обработчика на кнопку showMore

--- src/view/show-more-button.js
+++ src/view/show-more-button.js
@@ -7,7 +7,24 @@ const createShowMoreButtonTemplate = () => {
 };
 
 export default class ShowMoreButton extends AbstractView {
+  constructor() {
+    super();
+    this._clickHandler = this._clickHandler.bind(this); // привязываем обработчик к контексту(текущему классу)
+  }
+
   _getTemplate() {
     return createShowMoreButtonTemplate();
+  }
+
+  // функция-обработчик
+  _clickHandler(evt) {
+    evt.preventDefault();
+    this._callback.click(); // вызов функции-обработчика
+  }
+
+  // метод добавления обработчика на элемент
+  setClickHandler(callback) {
+    this._callback.click = callback; // записываем колбэк в объект с колбеками этого класса
+    this.getElement().addEventListener(`click`, this._clickHandler); // навешиваем обработчик на элемент
   }
 }
