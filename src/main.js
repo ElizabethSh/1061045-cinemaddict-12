54b175e8b18212e6a5232214848ca2180103da53
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-23 23:26:08 +0200

    Переносит в main.js из presenter/movie-list.js инициализацию рендеринга доп.разделов

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import ProfileView from "./view/profile.js";
 import MainNavigationView from "./view/main-navigation.js";
 import SortView from "./view/sort.js";
+import ContentView from "./view/content.js";
 import StatisticView from "./view/statistic.js";
+import ExtraListPresenter from "./presenter/extra-list.js";
 import MovieListPresenter from "./presenter/movie-list.js";
+import NoTaskView from "./view/no-film.js";
 import {generateFilm} from "./mock/film-card-mock";
 import {generateFilters} from "./mock/filter-mock";
 import {render, RenderPosition} from "./utils/render.js";
@@ -16,8 +19,18 @@ const footerStatistic = siteFooter.querySelector(`.footer__statistics`);
 
 const films = new Array(MAX_FILMS_CARD).fill().map(generateFilm);
 const filters = generateFilters(films);
+const topRatedFilms = films.slice();
+const mostCommentedFilms = films.slice();
 
-const movieListPresenter = new MovieListPresenter(siteMain);
+const contentComponent = new ContentView();
+const contentContainer = contentComponent.getElement();
+const movieListPresenter = new MovieListPresenter(contentContainer);
+const noTaskComponent = new NoTaskView();
+// console.log(films);
+const topRatedHeader = `Top Rated`;
+const mostCommentedHeader = `Most commented`;
+const topRatedFilmsPresenter = new ExtraListPresenter(contentContainer, topRatedHeader);
+const mostCommentedFilmsPresenter = new ExtraListPresenter(contentContainer, mostCommentedHeader);
 
 // HEADER
 render(siteHeader, new ProfileView(), RenderPosition.BEFOREEND);
@@ -29,8 +42,23 @@ render(siteMain, new MainNavigationView(filters), RenderPosition.BEFOREEND);
 // рендерит сортировку
 render(siteMain, new SortView(), RenderPosition.BEFOREEND);
 
-// рендерит контент с тремя разделами фильмов
+// рендерит контент
+if (films.length === 0) {
+  // если фильмов нет, рендерим заглушку
+  render(siteMain, noTaskComponent, RenderPosition.BEFOREEND);
+}
+
+// иначе отобрази контент с фильмами
+render(siteMain, contentComponent, RenderPosition.BEFOREEND);
+
+// рендерит контент с разделом всех фильмов
 movieListPresenter.init(films);
+
+// рендерит Most Commented раздел
+topRatedFilmsPresenter.init(topRatedFilms);
+
+// рендерит Top Rated раздел
+mostCommentedFilmsPresenter.init(mostCommentedFilms, mostCommentedHeader);
 
 // FOOTER
 render(footerStatistic, new StatisticView(films).getElement(), RenderPosition.BEFOREEND);
