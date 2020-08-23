9c7b86846347c3ec8392678c392c28d824d24eb5
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-23 23:36:57 +0200

    Добавляет в шаблон карточки фильма данные о рейтинге фильма из моков

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -1,13 +1,13 @@
 import AbstractView from "./abstract.js";
 
 const createFilmCardTemplate = (film) => {
-  const {title, description, poster} = film;
+  const {title, description, poster, rating} = film;
   const commentAmount = film.comments.length + 1;
 
   return (
     `<article class="film-card">
       <h3 class="film-card__title">${title}</h3>
-      <p class="film-card__rating">8.3</p>
+      <p class="film-card__rating">${rating}</p>
       <p class="film-card__info">
         <span class="film-card__year">1929</span>
         <span class="film-card__duration">1h 55m</span>
