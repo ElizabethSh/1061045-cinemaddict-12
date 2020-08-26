9fcfc30621426d07ad0ae5adbe0ff9048a635fbf
ElizabethSh <liza.shoshkina@gmail.com>
2020-08-26 14:23:59 +0200

    Добавляет данные о фильме и изменяет состояние чекбоксов в зависимости от данных

--- src/view/film-control.js
+++ src/view/film-control.js
@@ -1,22 +1,27 @@
 import AbstractView from "./abstract.js";
 
-const createFilmInfoControlsTemplate = () => {
+const createFilmInfoControlsTemplate = (film) => {
+  const {isWatchlist, isHistory, isFavorites} = film;
   return (
     `<section class="film-details__controls">
-      <input type="checkbox" class="film-details__control-input visually-hidden" id="watchlist" name="watchlist">
+      <input type="checkbox" class="film-details__control-input visually-hidden" id="watchlist" name="watchlist" ${isWatchlist ? `checked` : ``}>
       <label for="watchlist" class="film-details__control-label film-details__control-label--watchlist">Add to watchlist</label>
 
-      <input type="checkbox" class="film-details__control-input visually-hidden" id="watched" name="watched">
+      <input type="checkbox" class="film-details__control-input visually-hidden" id="watched" name="watched" ${isHistory ? `checked` : ``}>
       <label for="watched" class="film-details__control-label film-details__control-label--watched">Already watched</label>
 
-      <input type="checkbox" class="film-details__control-input visually-hidden" id="favorite" name="favorite">
+      <input type="checkbox" class="film-details__control-input visually-hidden" id="favorite" name="favorite" ${isFavorites ? `checked` : ``}>
       <label for="favorite" class="film-details__control-label film-details__control-label--favorite">Add to favorites</label>
     </section>`
   );
 };
 
 export default class FilmInfoControl extends AbstractView {
+  constructor(film) {
+    super();
+    this._film = film;
+  }
   _getTemplate() {
-    return createFilmInfoControlsTemplate();
+    return createFilmInfoControlsTemplate(this._film);
   }
 }
