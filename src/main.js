5b7a46b6e5219cdb9b65eb2c5ada3e6559c50616
ElizabethSh <liza.shoshkina@gmail.com>
2020-09-02 21:45:37 +0200

    Добавляет FilmsModel, записывает в него массив фильмов

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import ProfileView from "./view/profile.js";
 import StatisticView from "./view/statistic.js";
 import MovieListPresenter from "./presenter/movie-list.js";
+import FilmsModel from "./model/films.js";
 import {generateFilm} from "./mock/film-card-mock";
 import {render, RenderPosition} from "./utils/render.js";
 
@@ -12,14 +13,16 @@ const siteFooter = document.querySelector(`.footer`);
 const footerStatistic = siteFooter.querySelector(`.footer__statistics`);
 
 const films = new Array(MAX_FILMS_CARD).fill().map(generateFilm);
+const filmsModel = new FilmsModel();
+filmsModel.setFilms(films);
 
-const movieListPresenter = new MovieListPresenter(siteMain);
+const movieListPresenter = new MovieListPresenter(siteMain, filmsModel);
 
 // HEADER
 render(siteHeader, new ProfileView(), RenderPosition.BEFOREEND);
 
 // MAIN
-movieListPresenter.init(films);
+movieListPresenter.init();
 
 // FOOTER
 render(footerStatistic, new StatisticView(films).getElement(), RenderPosition.BEFOREEND);
