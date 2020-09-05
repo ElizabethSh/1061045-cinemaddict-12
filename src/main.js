73e689db362ceb2d898ef59a892ae44863b2b05b
ElizabethSh <liza.shoshkina@gmail.com>
2020-09-05 12:32:22 +0200

    Добавляет модели comments и filter, генерирует комментарии и передает их в model/comments.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import ProfileView from "./view/profile.js";
 import StatisticView from "./view/statistic.js";
 import MovieListPresenter from "./presenter/movie-list.js";
 import FilmsModel from "./model/films.js";
+import CommentsModel from "./model/comments.js";
+import FilterModel from "./model/filter.js";
 import {generateFilm} from "./mock/film-card-mock";
+import {generateComment} from "./mock/comment.js";
 import {render, RenderPosition} from "./utils/render.js";
 
 const MAX_FILMS_CARD = 11; // 11, чтобы было видно как работают фильтры
+const MAX_COMMENT_AMOUNT = 5;
 
 const siteHeader = document.querySelector(`.header`);
 const siteMain = document.querySelector(`.main`);
@@ -13,10 +17,25 @@ const siteFooter = document.querySelector(`.footer`);
 const footerStatistic = siteFooter.querySelector(`.footer__statistics`);
 
 const films = new Array(MAX_FILMS_CARD).fill().map(generateFilm);
+
+const comments = [];
+
+films.forEach((film) => {
+  const randomNumber = Math.round(Math.random() * MAX_COMMENT_AMOUNT);
+  const filmID = film.id;
+  const filmComments = new Array(randomNumber).fill().map(() => generateComment(filmID));
+  filmComments.forEach((comment) => comments.push(comment));
+});
+
 const filmsModel = new FilmsModel();
 filmsModel.setFilms(films);
 
-const movieListPresenter = new MovieListPresenter(siteMain, filmsModel);
+const commentsModel = new CommentsModel();
+commentsModel.setComments(comments);
+
+const filterModel = new FilterModel();
+
+const movieListPresenter = new MovieListPresenter(siteMain, filmsModel, commentsModel, filterModel);
 
 // HEADER
 render(siteHeader, new ProfileView(), RenderPosition.BEFOREEND);
