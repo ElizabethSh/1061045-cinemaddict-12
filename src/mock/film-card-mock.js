4147ad3dcec9527d3bfed1181162fbee68ebb7aa
ElizabethSh <liza.shoshkina@gmail.com>
2020-09-05 11:46:20 +0200

    Переносит функции генерации комментариев в отдельный модуль mock/comment.js

--- src/mock/film-card-mock.js
+++ src/mock/film-card-mock.js
@@ -1,14 +1,9 @@
-import {getRandomInteger} from "../utils/common.js";
-import {EMOJIS} from "../const.js";
+import {getRandomInteger, generateArrayElement} from "../utils/common.js";
 
 const MAX_SENTENCES = 5;
 const TEXT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus`;
-const MAX_DAY_GAP = 7;
 const MAX_FILM_DAY_GAP = 365;
-const MAX_COMMENT_AMOUNT = 5;
 const MAX_RATING = 10;
-
-// не стала переносить в модуль констант, чтобы потом не удалять оттуда
 
 const FILM_TITLES = [
   `The Dance of Life`,
@@ -30,22 +25,6 @@ const POSTERS = [
   `the-man-with-the-golden-arm.jpg`
 ];
 
-const MESSAGES = [
-  `Interesting setting and a good cast`,
-  `Booooooooooring`,
-  `Very very old. Meh`,
-  `Almost two hours? Seriously?`
-];
-
-const AUTHOR_NAMES = [
-  `Tim Macoveev`,
-  `John Doe`,
-];
-
-const generateArrayElement = (array) => {
-  return array[getRandomInteger(0, array.length - 1)];
-};
-
 const generateId = () => Date.now() + parseInt(Math.random() * 10000, 10);
 
 const generateDescription = () => {
@@ -65,14 +44,6 @@ const generateRating = () => {
   return (getRandomInteger(0, MAX_RATING - 1) + Math.random()).toFixed(1);
 };
 
-const generateCommentDate = () => {
-  const daysGap = getRandomInteger(0, MAX_DAY_GAP);
-  const currentDate = new Date();
-
-  currentDate.setDate(currentDate.getDate() - daysGap);
-  return new Date(currentDate);
-};
-
 const generateFilmDate = () => {
   const daysGap = getRandomInteger(0, MAX_FILM_DAY_GAP);
   const currentDate = new Date();
@@ -81,36 +52,11 @@ const generateFilmDate = () => {
   return new Date(currentDate);
 };
 
-const formatDate = (date) => {
-  const dd = date.getDate();
-  const mm = date.getMonth();
-  const year = date.getFullYear();
-  const hour = date.getHours();
-  const minutes = date.getMinutes();
-  return `${year}/${mm}/${dd} ${hour}:${minutes}`;
-};
-
-// функция генерации объекта комментария
-const generateComment = () => {
-  const date = formatDate(generateCommentDate());
-  const emoji = generateArrayElement(EMOJIS);
-  const commentMessage = generateArrayElement(MESSAGES);
-  const author = generateArrayElement(AUTHOR_NAMES);
-  return {
-    emoji: `${emoji}.png`,
-    commentMessage,
-    author,
-    date
-  };
-};
-
 // функция генерации объекта для описания фильма
 export const generateFilm = () => {
   const title = generateArrayElement(FILM_TITLES);
   const poster = generateArrayElement(POSTERS);
   const description = generateDescription();
-  const randomNumber = Math.round(Math.random() * MAX_COMMENT_AMOUNT);
-  const comments = new Array(randomNumber).fill().map(generateComment);
   const rating = generateRating();
   const releaseDate = generateFilmDate();
 
@@ -120,7 +66,6 @@ export const generateFilm = () => {
     poster,
     description,
     rating,
-    comments,
     releaseDate,
     isWatchlist: Boolean(getRandomInteger(0, 1)),
     isHistory: Boolean(getRandomInteger(0, 1)),
