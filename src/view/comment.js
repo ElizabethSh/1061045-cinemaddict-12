34802ed35e1abf2ef815cd439f9d62cb1f73571d
ElizabethSh <liza.shoshkina@gmail.com>
2020-09-05 21:11:59 +0200

    Удаляет создание шаблона раздела комментариев, добавляет создание шаблона отдельного комментария

--- src/view/comment.js
+++ src/view/comment.js
@@ -1,11 +1,10 @@
 import he from "he";
-import {EMOJIS} from "../const.js";
-import AbstractView from "./abstract.js";
+import Abstract from "./abstract.js";
 
 const createCommentItemTemplate = (comment) => {
   const {emoji, date, commentMessage, author} = comment;
-  return `
-    <li class="film-details__comment">
+  return (
+    `<li class="film-details__comment">
        <span class="film-details__comment-emoji">
          <img src="./images/emoji/${emoji}" width="55" height="55" alt="emoji-smile">
        </span>
@@ -17,82 +16,31 @@ const createCommentItemTemplate = (comment) => {
            <button class="film-details__comment-delete">Delete</button>
          </p>
        </div>
-     </li>
-  `;
-};
-
-const createEmojiItemTemplate = (emoji) => {
-  return `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emoji}" value="${emoji}">
-    <label class="film-details__emoji-label" for="emoji-${emoji}">
-      <img src="./images/emoji/${emoji}.png" width="30" height="30" alt="emoji-${emoji}">
-    </label>`;
-};
-
-const createEmojisTemplate = () => {
-  return EMOJIS.map((emoji) => createEmojiItemTemplate(emoji)).join(``);
-};
-
-const createFilmCommentsTemplate = (comments) => {
-
-  const createCommentsTemplate = () => {
-    return comments.map((comment) => createCommentItemTemplate(comment)).join(``);
-  };
-
-  const commentAmount = comments.length;
-  const commentTemplate = createCommentsTemplate();
-  const emojisTemplate = createEmojisTemplate();
-
-  return (
-    `<div class="form-details__bottom-container">
-      <section class="film-details__comments-wrap">
-        <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${commentAmount}</span></h3>
-
-        <ul class="film-details__comments-list">
-          ${commentTemplate}
-        </ul>
-
-        <div class="film-details__new-comment">
-          <div for="add-emoji" class="film-details__add-emoji-label"></div>
-
-          <label class="film-details__comment-label">
-            <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment"></textarea>
-          </label>
-
-          <div class="film-details__emoji-list">
-            ${emojisTemplate}
-          </div>
-        </div>
-      </section>
-    </div>`
+     </li>`
   );
 };
 
-export default class Comment extends AbstractView {
-  constructor(filmComments) {
+export default class Comment extends Abstract {
+  constructor(comment) {
     super();
-    this._filmComments = filmComments;
+    this._comment = comment;
 
-    this._emojiClickHandler = this._emojiClickHandler.bind(this);
-
-    this.getElement()
-        .querySelector(`.film-details__emoji-list`)
-        .addEventListener(`change`, this._emojiClickHandler);
+    this._commentDeleteClickHandler = this._commentDeleteClickHandler.bind(this);
   }
 
   _getTemplate() {
-    return createFilmCommentsTemplate(this._filmComments);
+    return createCommentItemTemplate(this._comment);
   }
 
-  _emojiClickHandler(evt) {
-    const userEmoji = this.getElement()
-                          .querySelector(`.film-details__add-emoji-label`);
-    const emoji = evt.target.value;
-
-    if (evt.target.tagName !== `INPUT`) {
-      return;
-    }
+  _commentDeleteClickHandler(evt) {
     evt.preventDefault();
+    this._callback.deleteClick();
+  }
 
-    userEmoji.innerHTML = `<img src="./images/emoji/${emoji}.png" width="60" height="60" alt="emoji-${emoji}">`;
+  setCommentDeleteClickHandler(callback) {
+    this._callback.deleteClick = callback;
+    this.getElement()
+        .querySelector(`.film-details__comment-delete`)
+        .addEventListener(`click`, this._commentDeleteClickHandler);
   }
 }
