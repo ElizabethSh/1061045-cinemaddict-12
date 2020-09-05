c7efabe3c15998813a6ec9046e997d7885c00118
ElizabethSh <liza.shoshkina@gmail.com>
2020-09-05 11:54:20 +0200

    Добавляет функцию генерации случайного элемента массива generateArrayElement

--- src/utils/common.js
+++ src/utils/common.js
@@ -14,26 +14,30 @@ export const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
+export const generateArrayElement = (array) => {
+  return array[getRandomInteger(0, array.length - 1)];
+};
+
 export const humanizeReleaseData = (date) => {
   return date.toLocaleString(`en-GB`, {day: `numeric`, month: `long`, year: `numeric`});
 };
 
-// метод обновления массива через замену изменившегося элемента
-export const updateItem = (items, update) => {
-  // ищем обновленный item в переданном массиве items
-  const index = items.findIndex((item) => item.id === update.id);
+// // метод обновления массива через замену изменившегося элемента
+// export const updateItem = (items, update) => {
+//   // ищем обновленный item в переданном массиве items
+//   const index = items.findIndex((item) => item.id === update.id);
 
-  // если такого item в массиве нет, верни исходный массив
-  if (index === -1) {
-    return items;
-  }
+//   // если такого item в массиве нет, верни исходный массив
+//   if (index === -1) {
+//     return items;
+//   }
 
-  // иначе перезапиши все items массива до обновленного item
-  // в нов.массив, добавь обновленный item, и после этого
-  // допиши оставшуюся часть массива туда же
-  return [
-    ...items.slice(0, index),
-    update,
-    ...items.slice(index + 1)
-  ];
-};
+//   // иначе перезапиши все items массива до обновленного item
+//   // в нов.массив, добавь обновленный item, и после этого
+//   // допиши оставшуюся часть массива туда же
+//   return [
+//     ...items.slice(0, index),
+//     update,
+//     ...items.slice(index + 1)
+//   ];
+// };
