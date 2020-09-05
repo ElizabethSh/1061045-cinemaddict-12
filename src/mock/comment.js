f7e7081f1c1ef574a2c1eb329e4416c81778575b
ElizabethSh <liza.shoshkina@gmail.com>
2020-09-05 11:43:28 +0200

    Выносит функции генерации комментариев в отдельный модуль mock/comment.js

---
+++ src/mock/comment.js
@@ -0,0 +1,48 @@
+import {getRandomInteger, generateArrayElement} from "../utils/common.js";
+import {EMOJIS} from "../const.js";
+
+const MAX_DAY_GAP = 7;
+
+const MESSAGES = [
+  `Interesting setting and a good cast`,
+  `Booooooooooring`,
+  `Very very old. Meh`,
+  `Almost two hours? Seriously?`
+];
+
+const AUTHOR_NAMES = [
+  `Tim Macoveev`,
+  `John Doe`,
+];
+
+const generateCommentDate = () => {
+  const daysGap = getRandomInteger(0, MAX_DAY_GAP);
+  const currentDate = new Date();
+
+  currentDate.setDate(currentDate.getDate() - daysGap);
+  return new Date(currentDate);
+};
+
+const formatDate = (date) => {
+  const dd = date.getDate();
+  const mm = date.getMonth();
+  const year = date.getFullYear();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  return `${year}/${mm}/${dd} ${hour}:${minutes}`;
+};
+
+// функция генерации объекта комментария
+export const generateComment = (id) => {
+  const date = formatDate(generateCommentDate());
+  const emoji = generateArrayElement(EMOJIS);
+  const commentMessage = generateArrayElement(MESSAGES);
+  const author = generateArrayElement(AUTHOR_NAMES);
+  return {
+    emoji: `${emoji}.png`,
+    commentMessage,
+    author,
+    date,
+    filmID: id
+  };
+};
