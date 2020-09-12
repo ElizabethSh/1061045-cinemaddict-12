c41ec3703956b2909f30b70c51f77c96c4df4cda
ElizabethSh <liza.shoshkina@gmail.com>
2020-09-12 21:21:38 +0200

    Добавляет утилитарные функции для форматирования описания фильма и даты релиза

--- src/utils/common.js
+++ src/utils/common.js
@@ -8,7 +8,35 @@ export const capitalizeFirstLetter = (string) => {
   return string[0].toUpperCase() + string.slice(1);
 };
 
+export const formatReleaseData = (date) => {
+  if (!(date instanceof Date)) {
+    return ``;
+  }
+  return moment(date).format(`DD MMMM YYYY`);
+};
+
+export const formatDescription = (descriptionText) => {
+  if (descriptionText.length < 140) {
+    return descriptionText;
+  }
+
+  return `${descriptionText.slice(0, 139)}...`;
+};
+
+// не забыть удалить!
+export const generateId = () => Date.now() + parseInt(Math.random() * 10000, 10);
+
+export const formatFilmDuration = (durationInMinutes) => {
+  return moment.utc().startOf(`day`).add({minutes: durationInMinutes}).format(`H[h] mm[m]`);
+};
+
+// не забыть удалить!
+export const generateArrayElement = (array) => {
+  return array[getRandomInteger(0, array.length - 1)];
+};
+
 // функция генерации случайного числа из диапозона
+// не забыть удалить!
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -16,36 +44,4 @@ export const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-export const generateArrayElement = (array) => {
-  return array[getRandomInteger(0, array.length - 1)];
-};
 
-export const formatReleaseData = (date) => {
-  if (!(date instanceof Date)) {
-    return ``;
-  }
-
-  return moment(date).format(`DD MMMM YYYY`);
-};
-
-export const generateId = () => Date.now() + parseInt(Math.random() * 10000, 10);
-
-// // метод обновления массива через замену изменившегося элемента
-// export const updateItem = (items, update) => {
-//   // ищем обновленный item в переданном массиве items
-//   const index = items.findIndex((item) => item.id === update.id);
-
-//   // если такого item в массиве нет, верни исходный массив
-//   if (index === -1) {
-//     return items;
-//   }
-
-//   // иначе перезапиши все items массива до обновленного item
-//   // в нов.массив, добавь обновленный item, и после этого
-//   // допиши оставшуюся часть массива туда же
-//   return [
-//     ...items.slice(0, index),
-//     update,
-//     ...items.slice(index + 1)
-//   ];
-// };
