66b4b6774fc81d715d45a5faf635551ffab7c947
ElizabethSh <liza.shoshkina@gmail.com>
2020-09-13 14:48:00 +0200

    Добавляет функцию для запроса данных с комментариями с сервера, на основе результата рендерит попап

--- src/presenter/film-card.js
+++ src/presenter/film-card.js
@@ -10,11 +10,12 @@ import {Mode, UserAction, UpdateType} from "../const.js";
 const body = document.querySelector(`body`);
 
 export default class FilmCard {
-  constructor(filmContainer, changeData, changeMode, commentsModel) {
+  constructor(filmContainer, changeData, changeMode, commentsModel, api) {
     this._filmCardContainer = filmContainer;
     this._changeData = changeData;
     this._changeMode = changeMode;
     this._commentsModel = commentsModel;
+    this._api = api;
 
     this._mode = Mode.DEFAULT;
 
@@ -34,12 +35,11 @@ export default class FilmCard {
 
   init(film) {
     this._film = film;
-    this._comments = this._commentsModel.getCommentsByFilmId(this._film.id);
 
     const prevFilmCardComponent = this._filmCardComponent;
     const prevPopupComponent = this._popupComponent;
 
-    this._filmCardComponent = new FilmCardView(film, this._comments);
+    this._filmCardComponent = new FilmCardView(film);
     this._popupComponent = new PopupView();
 
 
@@ -113,35 +113,50 @@ export default class FilmCard {
     render(this._filmCardContainer, this._filmCardComponent, RenderPosition.BEFOREEND);
   }
 
+  _getComments() {
+    this._api.getComments(this._film)
+      .then((comments) => {
+        this._commentsModel.setComments(UpdateType.INIT_POPUP, comments, this._film);
+      })
+      .catch(() => {
+        this._commentsModel.setComments(UpdateType.INIT_POPUP, []);
+      });
+  }
+
   // метод для рендера попапа
   _renderPopup() {
-    this._changeMode();
+    this._getComments();
+
+    // this._mode = Mode.POPUP;
+    // this._film.isPopupOpen = true;
+  }
+
+  renderPopupDetails(film) {
+    // рендер попапа без содержимого
     render(body, this._popupComponent, RenderPosition.BEFOREEND);
 
     document.addEventListener(`keydown`, this._escKeyDownHandler);
     this._popupComponent.setCloseButtonClickHandler(this._handleCloseButtonClick);
-    this._renderPopupDetails();
-
     this._mode = Mode.POPUP;
     this._film.isPopupOpen = true;
-  }
 
-  _renderPopupDetails() {
     this._filmInfoContainer = this._popupComponent.getElement().querySelector(`.form-details__top-container`);
-    this._renderFilmInfo();
-    this._renderFilmControl();
+
+    // рендер содержимого попапа
+    this._renderFilmInfo(film);
+    this._renderFilmControl(film);
     this._renderFilmComments();
   }
 
   // метод для рендеринга информации о фильме
-  _renderFilmInfo() {
-    const filmInfoComponent = new FilmInfoView(this._film);
+  _renderFilmInfo(film) {
+    const filmInfoComponent = new FilmInfoView(film);
     render(this._filmInfoContainer, filmInfoComponent, RenderPosition.BEFOREEND);
   }
 
   // метод для рендеринга кнопок под информацией о фильме
-  _renderFilmControl() {
-    const infoControlComponent = new FilmInfoControlView(this._film);
+  _renderFilmControl(film) {
+    const infoControlComponent = new FilmInfoControlView(film);
     render(this._filmInfoContainer, infoControlComponent, RenderPosition.BEFOREEND);
 
     infoControlComponent.setWatchlistChangeHandler(this._handleWatchlistClick);
@@ -151,9 +166,11 @@ export default class FilmCard {
 
   // метод для рендеринга комментариев
   _renderFilmComments() {
-    this._filmCommentsComponent = new FilmCommentsView(this._comments, this._film);
+    this._comments = this._commentsModel.getComments();
 
+    this._filmCommentsComponent = new FilmCommentsView(this._comments, this._film);
     this._commentsContainer = this._filmCommentsComponent.getElement().querySelector(`.film-details__comments-list`);
+
     this._filmCommentsComponent.setFormSubmitClickHandler(this._handleFormSubmit);
 
     // рендер секции комментариев к фильму
